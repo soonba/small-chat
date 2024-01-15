b3675039a02a4d873301e57d5d200cf57595f6a1
soonba <74886848+soonba@users.noreply.github.com>
2024-01-15 22:16:18 +0900

    Be/mongodb - 1  (#3)
    * be dockerfile 추가
    
    * mongoose connect

--- small-chat-backend/src/app.module.ts
+++ small-chat-backend/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MongooseModule.forRoot('mongodb://mongodb/nest')],
+  controllers: [AppController],
 })
 export class AppModule {}
