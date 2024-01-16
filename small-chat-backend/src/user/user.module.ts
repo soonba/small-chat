fd1bac642e5874cfc2bdb0f3ed2a9eeb54d80573
sunba <sunba2318@gmail.com>
2024-01-16 20:58:45 +0900

    modules

--- small-chat-backend/src/user/user.module.ts
+++ small-chat-backend/src/user/user.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserResolver, UserService],
+})
 export class UserModule {}
