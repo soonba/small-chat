96f364acda8efab66fab64e5342e706dfb99b391
sunba <sunba2318@gmail.com>
2024-01-17 21:13:25 +0900

    바뀐 포트 반영, 스펙 정리, 디버깅

--- small-chat-client/src/pages/Login/Login.tsx
+++ small-chat-client/src/pages/Login/Login.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { InformationCircleIcon } from '@heroicons/react/20/solid';
 
-import { useJoinMutation } from 'generated/graphql';
+import { useJoinMutation, useSubscribeRoomSubscription } from 'generated/graphql';
 
 export default function Login() {
     const [nickname, setNickname] = useState('');
@@ -16,6 +16,22 @@ export default function Login() {
         }
     });
 
+    useSubscribeRoomSubscription({
+        variables: {
+            input: {
+                roomIds: ['1']
+            }
+        },
+        onData({ data: { data } }) {
+            console.log('도착111');
+            // eslint-disable-next-line no-console
+            console.log(data);
+        },
+        onError(error) {
+            console.log(error);
+        }
+    });
+
     // TODO: API
     const handleClick = () => {
         if (nickname) {
@@ -24,37 +40,37 @@ export default function Login() {
     };
 
     return (
-        <div className="flex h-full w-full items-center justify-center">
-            <div className="bg-blue-gray-50 flex h-max min-h-40 min-w-[320px] items-center justify-center gap-x-2 rounded-lg p-5 shadow-md">
-                <div className="flex flex-col">
-                    <label htmlFor="nickname" className="text-blue-gray-600 ml-1 flex flex-col justify-center text-xs font-bold">
-                        닉네임
-                        <input
-                            id="nickname"
-                            type="text"
-                            maxLength={20}
-                            value={nickname}
-                            onChange={(e) => {
-                                if (e.currentTarget.type === 'text') {
-                                    if (e.currentTarget.maxLength && e.currentTarget.value.length > e.currentTarget.maxLength) {
-                                        // 한글 글자수 제한
-                                        e.currentTarget.value = e.currentTarget.value.slice(0, e.currentTarget.maxLength);
-                                    }
+      <div className="flex h-full w-full items-center justify-center">
+          <div className="flex h-max min-h-40 min-w-[320px] items-center justify-center gap-x-2 rounded-lg bg-blue-gray-50 p-5 shadow-md">
+              <div className="flex flex-col">
+                  <label htmlFor="nickname" className="ml-1 flex flex-col justify-center text-xs font-bold text-blue-gray-600">
+                      닉네임
+                      <input
+                        id="nickname"
+                        type="text"
+                        maxLength={20}
+                        value={nickname}
+                        onChange={(e) => {
+                            if (e.currentTarget.type === 'text') {
+                                if (e.currentTarget.maxLength && e.currentTarget.value.length > e.currentTarget.maxLength) {
+                                    // 한글 글자수 제한
+                                    e.currentTarget.value = e.currentTarget.value.slice(0, e.currentTarget.maxLength);
                                 }
-                                setNickname(e.currentTarget.value);
-                            }}
-                            className="border-blue-gray-200 text-blue-gray-900 hover:border-blue-gray-900 focus:border-blue-gray-900 h-10 min-w-56 truncate rounded-md border bg-white p-2 text-sm font-medium outline-none ring-0 hover:border-2 focus:border-2"
-                        />
-                    </label>
-                    <small className="text-blue-gray-600 ml-1 mt-1 flex items-center gap-1 text-xs font-normal leading-6">
-                        <InformationCircleIcon className="h-4 w-4" />
-                        최대 20자까지 입력할 수 있습니다.
-                    </small>
-                </div>
-                <button type="button" onClick={handleClick} className="bg-blue-gray-900 text-blue-gray-50 mb-7 mt-4 h-10 w-16 whitespace-nowrap rounded-md text-xs font-bold hover:opacity-80">
-                    등록
-                </button>
-            </div>
-        </div>
+                            }
+                            setNickname(e.currentTarget.value);
+                        }}
+                        className="h-10 min-w-56 truncate rounded-md border border-blue-gray-200 bg-white p-2 text-sm font-medium text-blue-gray-900 outline-none ring-0 hover:border-2 hover:border-blue-gray-900 focus:border-2 focus:border-blue-gray-900"
+                      />
+                  </label>
+                  <small className="ml-1 mt-1 flex items-center gap-1 text-xs font-normal leading-6 text-blue-gray-600">
+                      <InformationCircleIcon className="h-4 w-4" />
+                      최대 20자까지 입력할 수 있습니다.
+                  </small>
+              </div>
+              <button type="button" onClick={handleClick} className="mb-7 mt-4 h-10 w-16 whitespace-nowrap rounded-md bg-blue-gray-900 text-xs font-bold text-blue-gray-50 hover:opacity-80">
+                  등록
+              </button>
+          </div>
+      </div>
     );
 }
