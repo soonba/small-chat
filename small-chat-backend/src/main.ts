f36af62c4d2674dc1e8e3ca0e40559775db28adb
sunba <sunba2318@gmail.com>
2024-01-18 20:22:09 +0900

    누락코드 복구

--- small-chat-backend/src/main.ts
+++ small-chat-backend/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000',
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
