f4cc6df65dc956bc36d250ee5d00a8049f2b4800
sunba <sunba2318@gmail.com>
2024-01-23 22:04:18 +0900

    3. 구독과 조회는 ChatList 컴포넌트에서

--- small-chat-client/src/pages/Chat/Chat.tsx
+++ small-chat-client/src/pages/Chat/Chat.tsx
@@ -2,38 +2,19 @@ import { useCallback, useState } from 'react';
 
 import ChatList from './components/ChatList';
 import ChatRoom from './components/ChatRoom/ChatRoom';
-import { ParticipationRoom, useGetMyChattingListQuery } from '../../generated/graphql';
 
 export default function Chat() {
     const [selected, setSelected] = useState('');
-    const [rooms, setRooms] = useState([{ roomId: '', roomName: '' } as ParticipationRoom]);
 
     const handleChatRoomSelect = useCallback((id: string) => setSelected(id), []);
     const handleChatRoomLeave = useCallback(() => setSelected(''), []);
 
     const userId = localStorage.getItem('userId') || '';
-    useGetMyChattingListQuery({
-        fetchPolicy: 'no-cache',
-        variables: { input: { userId } },
-        onCompleted({ getMyChattingList: { participationRooms } }) {
-            setRooms(participationRooms);
-        }
-    });
-
-    // 리스트 구독은 v1.1
-    // useSubscribeRoomSubscription({
-    //     variables: { input: { roomIds: rooms.map((el) => el.roomId) } },
-    //     onData({ data: { data } }) {
-    //         const messageResponse = data?.subscribeRoom;
-    //         const { sender, roomId, message: _message } = messageResponse ?? { sender: '', roomId: '', message: '' };
-    //         setMessage({ sender, roomId, message: _message });
-    //     }
-    // });
 
     /* 메시지를 받으면 sender: userId 검사를 통해 누구인지 판단.. */
     return (
         <>
-            <ChatList rooms={rooms} onClick={handleChatRoomSelect} />
+            <ChatList userId={userId} onClick={handleChatRoomSelect} />
             <ChatRoom selected={selected} onLeave={handleChatRoomLeave} />{' '}
         </>
     );
--- small-chat-client/src/pages/Chat/components/ChatList/ChatList.tsx
+++ small-chat-client/src/pages/Chat/components/ChatList/ChatList.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+
 import ChatListItem from './ChatListItem';
-import { ParticipationRoom } from '../../../../generated/graphql';
+import { ParticipationRoom, useGetMyChattingListQuery } from '../../../../generated/graphql';
 
 type ChatListType = {
-    rooms: ParticipationRoom[];
+    userId: string;
     onClick: (id: string) => void;
 };
 
-export default function ChatList({ rooms, onClick }: ChatListType) {
+export default function ChatList({ userId, onClick }: ChatListType) {
+    const [rooms, setRooms] = useState([{ roomId: '', roomName: '' } as ParticipationRoom]);
+    useGetMyChattingListQuery({
+        fetchPolicy: 'no-cache',
+        variables: { input: { userId } },
+        onCompleted({ getMyChattingList: { participationRooms } }) {
+            setRooms(participationRooms);
+        }
+    });
+
+    // 리스트 구독은 v1.1
+    // useSubscribeRoomSubscription({
+    //     variables: { input: { roomIds: rooms.map((el) => el.roomId) } },
+    //     onData({ data: { data } }) {
+    //         const messageResponse = data?.subscribeRoom;
+    //         const { sender, roomId, message: _message } = messageResponse ?? { sender: '', roomId: '', message: '' };
+    //         setMessage({ sender, roomId, message: _message });
+    //     }
+    // });
     return (
         <div className="fixed bottom-0 left-0 top-16 w-96 py-2 shadow-md">
             <ul className="max-h-[calc(100vh-64px)] w-full space-y-5 overflow-y-auto p-5 custom-scroll">
