01b7ec2f84a8daf62aa6b684d915098f881581e7
sunba <sunba2318@gmail.com>
2024-03-19 22:04:05 +0900

    wip - Login.tsx

--- chat-client/src/pages/Login/Login.tsx
+++ chat-client/src/pages/Login/Login.tsx
@@ -3,22 +3,15 @@ import { useNavigate } from 'react-router-dom';
 
 import { InformationCircleIcon } from '@heroicons/react/20/solid';
 
-import { useJoinMutation } from 'generated/graphql';
-
 export default function Login() {
-    const [nickname, setNickname] = useState('');
+    const [userId, setUserId] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
     const navigate = useNavigate();
 
-    const [joinMutation] = useJoinMutation({
-        onCompleted({ join: { userId, nickname: _nickname } }) {
-            localStorage.setItem('userId', userId);
-            navigate('/chat');
-        }
-    });
-
+    // todo join || login
     const handleClick = () => {
-        if (nickname) {
-            joinMutation({ variables: { input: { nickname } } });
+        if (userId && password) {
+            // joinMutation({ variables: { input: { userId, password } } });
         }
     };
 
@@ -27,12 +20,12 @@ export default function Login() {
             <div className="flex h-max min-h-40 min-w-[320px] items-center justify-center gap-x-2 rounded-lg bg-blue-gray-50 p-5 shadow-md">
                 <div className="flex flex-col">
                     <label htmlFor="nickname" className="ml-1 flex flex-col justify-center text-xs font-bold text-blue-gray-600">
-                        닉네임
+                        ID
                         <input
-                            id="nickname"
+                            id="userId"
                             type="text"
                             maxLength={20}
-                            value={nickname}
+                            value={userId}
                             onChange={(e) => {
                                 if (e.currentTarget.type === 'text') {
                                     if (e.currentTarget.maxLength && e.currentTarget.value.length > e.currentTarget.maxLength) {
@@ -40,7 +33,26 @@ export default function Login() {
                                         e.currentTarget.value = e.currentTarget.value.slice(0, e.currentTarget.maxLength);
                                     }
                                 }
-                                setNickname(e.currentTarget.value);
+                                setUserId(e.currentTarget.value);
+                            }}
+                            className="h-10 min-w-56 truncate rounded-md border border-blue-gray-200 bg-white p-2 text-sm font-medium text-blue-gray-900 outline-none ring-0 hover:border-2 hover:border-blue-gray-900 focus:border-2 focus:border-blue-gray-900"
+                        />
+                    </label>
+                    <label htmlFor="nickname" className="ml-1 flex flex-col justify-center text-xs font-bold text-blue-gray-600">
+                        Password
+                        <input
+                            id="password"
+                            type="text"
+                            maxLength={20}
+                            value={userId}
+                            onChange={(e) => {
+                                if (e.currentTarget.type === 'text') {
+                                    if (e.currentTarget.maxLength && e.currentTarget.value.length > e.currentTarget.maxLength) {
+                                        // 한글 글자수 제한
+                                        e.currentTarget.value = e.currentTarget.value.slice(0, e.currentTarget.maxLength);
+                                    }
+                                }
+                                setPassword(e.currentTarget.value);
                             }}
                             className="h-10 min-w-56 truncate rounded-md border border-blue-gray-200 bg-white p-2 text-sm font-medium text-blue-gray-900 outline-none ring-0 hover:border-2 hover:border-blue-gray-900 focus:border-2 focus:border-blue-gray-900"
                         />
@@ -50,8 +62,8 @@ export default function Login() {
                         최대 20자까지 입력할 수 있습니다.
                     </small>
                 </div>
-                <button type="button" onClick={handleClick} className="mb-7 mt-4 h-10 w-16 whitespace-nowrap rounded-md bg-blue-gray-900 text-xs font-bold text-blue-gray-50 hover:opacity-80">
-                    등록
+                <button type="button" onClick={handleClick} className="mb-0 mt-11 h-10 w-16 whitespace-nowrap rounded-md bg-blue-gray-900 text-xs font-bold text-blue-gray-50 hover:opacity-80">
+                    로그인
                 </button>
             </div>
         </div>
