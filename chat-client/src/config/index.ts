0541b820e666971c34d785772890d30fb519064a
sunba <sunba2318@gmail.com>
2024-03-19 21:55:40 +0900

    fe - react query 패키지 추가, be config추가

--- chat-client/src/config/index.ts
+++ chat-client/src/config/index.ts
@@ -3,7 +3,8 @@ const appConfig = {
     appEnv: process.env.REACT_APP_ENV,
     graphqlUrl: process.env.REACT_APP_GRAPHQL_URL,
     graphqlSubscriptionUrl: process.env.REACT_APP_GRAPHQL_SUBSCRIPTION_URL,
-    graphqlApiUrl: process.env.REACT_APP_GRAPHQL_API_URL
+    graphqlApiUrl: process.env.REACT_APP_GRAPHQL_API_URL,
+    authApiUrl: process.env.AUTH_API_URL
 };
 
 export default appConfig;
