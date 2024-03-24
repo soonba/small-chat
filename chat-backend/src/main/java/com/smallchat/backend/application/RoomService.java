3ad9d6153dca44905573572f54c53ad051836d91
sunba <sunba2318@gmail.com>
2024-03-24 20:04:31 +0900

    wip rooms

--- chat-backend/src/main/java/com/smallchat/backend/application/RoomService.java
+++ chat-backend/src/main/java/com/smallchat/backend/application/RoomService.java
@@ -1,13 +1,31 @@
 package com.smallchat.backend.application;
 
-import com.smallchat.backend.persistance.RoomRepository;
+import com.smallchat.backend.data.room.Rooms;
+import com.smallchat.backend.domain.User;
+import com.smallchat.backend.domain.UserRoom;
+import com.smallchat.backend.persistance.UserRepository;
+import com.smallchat.backend.persistance.UserRoomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RoomService {
-    private final RoomRepository roomRepository;
+    private final UserRoomRepository userRoomRepository;
+    private final UserRepository userRepository;
 
-    public RoomService(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
+    public RoomService(UserRoomRepository userRoomRepository, UserRepository userRepository) {
+        this.userRoomRepository = userRoomRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    public Rooms findRoomListByUser(UUID id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("찾을 수 없는 아이디"));
+        List<UserRoom> userRoomByUser = userRoomRepository.findUserRoomByUser(user);
+//        userRoomByUser.stream().map(userRoom -> new Room())
+        //todo wip
+        return new Rooms(List.of(null));
     }
 }
--- chat-backend/src/main/java/com/smallchat/backend/presentation/RoomController.java
+++ chat-backend/src/main/java/com/smallchat/backend/presentation/RoomController.java
@@ -1,11 +1,14 @@
 package com.smallchat.backend.presentation;
 
 import com.smallchat.backend.application.RoomService;
+import com.smallchat.backend.application.TokenService;
 import com.smallchat.backend.data.dto.ApiResponse;
-import com.smallchat.backend.data.dto.CheckUserDuplicationDto;
+import com.smallchat.backend.data.dto.RoomListDto;
+import com.smallchat.backend.data.jwt.JwtPayload;
+import com.smallchat.backend.data.room.Rooms;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/rooms")
 public class RoomController {
     private final RoomService roomService;
+    private final TokenService tokenService;
 
-    public RoomController(RoomService roomService) {
+    public RoomController(RoomService roomService, TokenService tokenService) {
         this.roomService = roomService;
+        this.tokenService = tokenService;
     }
 
-    @GetMapping("")
-    public ResponseEntity<ApiResponse<CheckUserDuplicationDto.Response>> checkAccountExists(@PathVariable String accountId) {
-//        ApiResponse<CheckUserDuplicationDto.Response> exists = roomService.checkAccountIdExists(accountId);
-        return ResponseEntity.ok(new ApiResponse<>(new CheckUserDuplicationDto.Response(true)));
+    @GetMapping()
+    public ResponseEntity<ApiResponse<RoomListDto.Response>> getParticipationRoomList(@RequestHeader("Authorization") String accessToken) {
+        JwtPayload jwtPayload = tokenService.compile(accessToken);
+        Rooms rooms = roomService.findRoomListByUser(jwtPayload.id());
+        return ResponseEntity.ok(new ApiResponse<>(new RoomListDto.Response(rooms)));
     }
 }
