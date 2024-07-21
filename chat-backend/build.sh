6cc8ba9d9d9901535599d4718e4586265b7d36dd
sunba <sunba2318@gamil.com>
2024-07-21 13:38:55 +0900

    wip

--- chat-backend/build.sh
+++ chat-backend/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./gradlew clean bootJar
-docker build -t seonba/chat-backend .
+docker build --platform linux/amd64 -t seonba/chat-backend .
 docker rmi $(docker images -f "dangling=true" -q)
