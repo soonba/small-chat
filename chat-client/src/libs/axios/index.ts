5986602aa1d3ab574403a12f10179b76a5b64ad1
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-07-23 18:36:22 +0900

    WIP

--- chat-client/src/libs/axios/index.ts
+++ chat-client/src/libs/axios/index.ts
@@ -1,10 +1,12 @@
+import { AxiosResponse } from 'axios';
+
 import instance from './setup';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getData = async <ReturnType, QueryParamType = any>(
     url: string,
     params?: QueryParamType
-): Promise<ReturnType> => instance.get<ReturnType>(`${url}`, { params }).then((res) => res.data);
+): Promise<AxiosResponse<ReturnType>> => instance.get<ReturnType>(`${url}`, { params }).then((res) => res);
 
-export const postData = async <ReturnType, BodyType>(url: string, body: BodyType): Promise<ReturnType> =>
-    instance.post(`${url}`, body).then((res) => res.data);
+export const postData = async <ReturnType, BodyType>(url: string, body: BodyType): Promise<AxiosResponse<ReturnType>> =>
+    instance.post(`${url}`, body).then((res) => res);
