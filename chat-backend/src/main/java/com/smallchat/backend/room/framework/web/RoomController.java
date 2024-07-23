11e2534d204bb94f80bb9c464c9f711d7c13bb99
sunba <sunba2318@gamil.com>
2024-07-23 18:48:06 +0900

    wip

--- chat-backend/src/main/java/com/smallchat/backend/room/framework/web/RoomController.java
+++ chat-backend/src/main/java/com/smallchat/backend/room/framework/web/RoomController.java
@@ -45,7 +45,6 @@ public class RoomController {
     @GetMapping()
     public ResponseEntity<ApiResponse<RoomBasicInfoListDto.Response>> getChattingRoomList(@RequestHeader("Authorization") String authorization) {
         TokenPayload tokenPayload = jwtProvider.parseFromBearer(authorization);
-        System.out.println(tokenPayload.userId());
         RoomBasicInfoListDto.Response response = participatingRoomsUseCase.getChattingRoomList(tokenPayload.userId());
         return ResponseEntity.ok(new ApiResponse<>(response));
     }
