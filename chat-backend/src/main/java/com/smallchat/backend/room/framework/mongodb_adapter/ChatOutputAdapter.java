c677725866b1a7c5c403495e3d6c2dd6ceb90ebf
sunba <sunba2318@gamil.com>
2024-07-23 19:28:07 +0900

    테스트전 - getLastChat

--- chat-backend/src/main/java/com/smallchat/backend/room/framework/mongodb_adapter/ChatOutputAdapter.java
+++ chat-backend/src/main/java/com/smallchat/backend/room/framework/mongodb_adapter/ChatOutputAdapter.java
@@ -3,6 +3,12 @@ package com.smallchat.backend.room.framework.mongodb_adapter;
 import com.smallchat.backend.room.application.outputport.ChatOutputPort;
 import com.smallchat.backend.room.domain.model.vo.Chat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +18,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ChatOutputAdapter implements ChatOutputPort {
     private final ChatRepository chatRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public void save(Chat chat) {
@@ -25,10 +32,11 @@ public class ChatOutputAdapter implements ChatOutputPort {
 
     @Override
     public List<Chat> getLastChatInfo(List<UUID> roomIdList) {
-//        Aggregation.newAggregation(Chat.class, Aggregation.sort(Aggregation.sort(Aggregation..DESC, "timestamp")),
-//                Aggregation.group("roomId")
-//                        .first(Aggregation.ROOT).as("lastChat"));
-        //todo
-        return List.of();
+        MatchOperation matchStage = Aggregation.match(Criteria.where("roomId").in(roomIdList));
+        Aggregation aggregation = Aggregation.newAggregation(matchStage,
+                Aggregation.sort(Sort.Direction.DESC, "createAt"),
+                Aggregation.group("roomId").first(Aggregation.ROOT).as("lastChat"));
+        AggregationResults<Chat> aggregate = mongoTemplate.aggregate(aggregation, Chat.class, Chat.class);
+        return aggregate.getMappedResults();
     }
 }
