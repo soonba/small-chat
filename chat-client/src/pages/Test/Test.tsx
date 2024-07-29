12649e6d1ef01add966c76c77d75498aa9fb36f5
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-07-29 17:26:57 +0900

    socket WIP

---
+++ chat-client/src/pages/Test/Test.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+import { socket } from 'libs/socket';
+
+export default function Test() {
+    const [isConnected, setIsConnected] = useState(socket.connected);
+
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true);
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+        };
+    }, []);
+
+    return <div>{isConnected ? 'connected' : 'not connected'}</div>;
+}
