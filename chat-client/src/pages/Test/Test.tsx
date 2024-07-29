5345f9986da71a36a39cff37aa6bb0cfb2a80b41
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-07-29 19:41:18 +0900

    WIP

--- chat-client/src/pages/Test/Test.tsx
+++ chat-client/src/pages/Test/Test.tsx
@@ -23,5 +23,23 @@ export default function Test() {
         };
     }, []);
 
-    return <div>{isConnected ? 'connected' : 'not connected'}</div>;
+    const [value, setValue] = useState('');
+
+    const handleSubmit = () => {
+        socket.emit('chat message', value);
+    };
+
+    return (
+        <div className="flex min-h-screen w-full flex-col items-center justify-center bg-white">
+            <h1>{isConnected ? 'connected' : 'not connected'}</h1>
+            <textarea
+                value={value}
+                onChange={(e) => setValue(e.currentTarget.value)}
+                className="h-80 w-1/2 resize-none border border-black"
+            />
+            <button type="button" onClick={handleSubmit} className="mt-10 w-1/2 rounded border border-black py-5">
+                테스트
+            </button>
+        </div>
+    );
 }
