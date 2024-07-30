4ec42b2814d8107d8a00ba6ec3237ddb6f463cd6
soonba <sunba2318@gmail.com>
2024-07-30 17:55:00 +0900

    게이트웨이 수정

--- chat-service/src/chat/domain/model/event.type.ts
+++ chat-service/src/chat/domain/model/event.type.ts
@@ -1,5 +1,12 @@
+//이 파일 이름변경?
 export enum EventType {
   SUBSCRIBE = 'subscribe',
   UN_SUBSCRIBE = 'unSubscribe',
   MESSAGE = 'message',
+}
+
+export enum PropertyKey {
+  CHAT_IDS = 'chatIds',
+  CHAT_ID = 'chatId',
+  MESSAGE = 'messageBody',
 }
