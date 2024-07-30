b2e68c7b35e6d328b1e76dd230a22c8ba9d75eaf
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-07-30 17:36:07 +0900

    WIP

--- chat-client/src/index.tsx
+++ chat-client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { ApolloProvider } from '@apollo/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { store } from 'libs/redux/store';
