f572107815458a4850f870fcfd88eeb3fd315130
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-07-30 20:27:37 +0900

    WIP

--- chat-client/src/layout/BaseLayout.tsx
+++ chat-client/src/layout/BaseLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 
+import { useGetMyInfo } from 'services/auth';
+
 import Footer from './Footer';
 import Header from './Header';
 
 export default function BaseLayout() {
+    useGetMyInfo();
+
     return (
         <div className="min-h-full w-full">
             <Header />
