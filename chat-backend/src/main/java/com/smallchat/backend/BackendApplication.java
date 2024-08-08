5fb27539a902f9db711245cc54f80c30a964f7db
soonba <sunba2318@gmail.com>
2024-08-08 21:25:41 +0900

    minor change

--- chat-backend/src/main/java/com/smallchat/backend/BackendApplication.java
+++ chat-backend/src/main/java/com/smallchat/backend/BackendApplication.java
@@ -9,11 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
-        System.out.println("최신버전");
-        System.out.println("최신버전");
-        System.out.println("최신버전");
-        System.out.println("최신버전");
-        System.out.println("최신버전");
         SpringApplication.run(BackendApplication.class, args);
     }
 
