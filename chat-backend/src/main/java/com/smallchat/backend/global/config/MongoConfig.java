85e71e772c4f326c7e27198d4573f6e7ba31b54c
soonba <sunba2318@gmail.com>
2024-08-11 11:40:45 +0900

    fix : mongoDB find UTC

--- chat-backend/src/main/java/com/smallchat/backend/global/config/MongoConfig.java
+++ chat-backend/src/main/java/com/smallchat/backend/global/config/MongoConfig.java
@@ -1,21 +1,41 @@
 package com.smallchat.backend.global.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.smallchat.backend.global.config.converter.ZonedDateTimeReadConverter;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
     private static final String DATABASE_NAME = "small_chat";
-    private ZonedDateTimeReadConverter zonedDateTimeReadConverter;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String DATABASE_URL;
+    private final ZonedDateTimeReadConverter zonedDateTimeReadConverter;
+
 
     @Override
     protected String getDatabaseName() {
         return DATABASE_NAME;
+    }
+
+    @Bean
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(DATABASE_URL);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
     }
 
     @Override
