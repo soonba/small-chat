1b298767f2dfaa62da9e44e944693b9d11c4ca5a
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-08-12 18:36:35 +0900

    WIP

--- chat-client/src/libs/socket/useSocket.tsx
+++ chat-client/src/libs/socket/useSocket.tsx
@@ -1,6 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import dayjs from 'dayjs';
+import { useQueryClient } from '@tanstack/react-query';
+
+import { chatKeys } from 'utils/queryKey';
 
 import { socket } from './setup';
 
@@ -23,7 +26,19 @@ type MessageBodyType = {
     messageBody: MessageType;
 };
 
+type ChatListType = {
+    chatBasicInfos: {
+        chatId: string;
+        chatName: string;
+        lastMessage: string;
+        lastMessageTime: string;
+    }[];
+};
+
 const useSocket = () => {
+    const queryClient = useQueryClient();
+    const { id } = useParams();
+
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [message, setMessage] = useState<SocketMessageType[]>([]);
 
@@ -69,16 +84,26 @@ const useSocket = () => {
 
     const onMessageReceive = useCallback(() => {
         socket.on(EventType.MESSAGE, (message: MessageType & { createdAt: string }) => {
-            console.log(dayjs().toDate());
             if (message.chatId.startsWith('list_')) {
-                console.log('list message');
-            } else {
-                console.log('chat message');
+                const chatId = message.chatId.replace('list_', '');
+                queryClient.setQueryData(chatKeys.lists(), (previousList: ChatListType | undefined) => {
+                    if (previousList?.chatBasicInfos) {
+                        const newList = previousList.chatBasicInfos.slice(0);
+                        const index = newList.findIndex((data) => data.chatId === chatId);
+                        newList[index] = {
+                            ...newList[index],
+                            lastMessage: message.message,
+                            lastMessageTime: message.createdAt
+                        };
+                        return { chatBasicInfos: newList };
+                    }
+                    return previousList;
+                });
+            } else if (message.chatId === id) {
                 setMessage((prev) => [message, ...prev]);
             }
-            console.log(message);
         });
-    }, []);
+    }, [id]);
 
     const onCurrentChatLeave = useCallback((chatId: string) => {
         socket.emit(EventType.UN_SUBSCRIBE, { chatId });
