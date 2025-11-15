c7297f10c30a9dacb85a20959741b726b254c4db
soonba <sunba2318@gmail.com>
2025-11-15 10:37:18 +0900

    refactor: main

--- chat-backend/src/main/java/com/smallchat/backend/BackendApplication.kt
+++ chat-backend/src/main/java/com/smallchat/backend/BackendApplication.kt
@@ -1,6 +1,5 @@
 package com.smallchat.backend
 
-import com.smallchat.backend.global.config.RabbitMQConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -8,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 open class BackendApplication
 
 fun main(args: Array<String>) {
-    val rabbitMQConfig = RabbitMQConfig()
-    rabbitMQConfig.someFunction()
     SpringApplication.run(BackendApplication::class.java, *args)
 }
