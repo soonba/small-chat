b2017142322ed1720d9b3256942114d3f88338e4
soonba <sunba2318@gmail.com>
2025-11-16 17:03:07 +0900

    mod: Refresh

--- chat-backend/src/main/java/com/smallchat/backend/user/application/usecase/TokenUseCase.java
+++ chat-backend/src/main/java/com/smallchat/backend/user/application/usecase/TokenUseCase.java
@@ -3,36 +3,21 @@ package com.smallchat.backend.user.application.usecase;
 
 import com.smallchat.backend.global.utils.AuthenticatedUser;
 import com.smallchat.backend.global.utils.JwtProvider;
-import com.smallchat.backend.global.utils.Tokens;
 import com.smallchat.backend.user.application.inputport.TokenInputPort;
 import com.smallchat.backend.user.application.outputport.UserOutputPort;
 import com.smallchat.backend.user.domain.model.User;
 import com.smallchat.backend.user.interfaces.web.dto.FetchMeDto;
-import com.smallchat.backend.user.interfaces.web.dto.RefreshDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Deprecated
 public class TokenUseCase implements TokenInputPort {
 
     private final UserOutputPort userOutputPort;
     private final JwtProvider jwtProvider;
 
-    @Override
-    public RefreshDto.Response refresh(RefreshDto.Request refreshDto) {
-        String rt = refreshDto.refreshToken();
-        AuthenticatedUser authenticatedUser = jwtProvider.parseToken(rt);
-
-        String id = authenticatedUser.getUserId();
-        String nickname = authenticatedUser.getNickname();
-        userOutputPort.validateRefreshToken(id, rt);
-
-        Tokens tokens = jwtProvider.createTokens(id, nickname);
-        userOutputPort.saveRefreshToken(id, tokens.refreshToken());
-
-        return new RefreshDto.Response(tokens);
-    }
 
     @Override
     public FetchMeDto.Response fetchMe(AuthenticatedUser authenticatedUser) {
