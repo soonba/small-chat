22324d0702707b0f611a50b05b04f3ac8ab284c7
soonba <sunba2318@gmail.com>
2025-11-17 22:07:11 +0900

    mod : Logout, FetchMe, Id Validation

--- chat-backend/src/main/java/com/smallchat/backend/user/application/inputport/ValidateUserInputPort.java
+++ chat-backend/src/main/java/com/smallchat/backend/user/application/inputport/ValidateUserInputPort.java
@@ -1,9 +1,5 @@
 package com.smallchat.backend.user.application.inputport;
 
-import com.smallchat.backend.user.interfaces.web.dto.CheckUserDuplicationDto;
-
 public interface ValidateUserInputPort {
-    CheckUserDuplicationDto.Response isExistId(String id);
-
     void hasReachedMaxChatLimit(String userId);
 }
